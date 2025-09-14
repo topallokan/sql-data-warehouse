b9d0a20f3e3b98a9a938e8b29c00455bf867da29
Okan Topal <79374662+topallokan@users.noreply.github.com>
2025-09-15 02:18:02 +0300

    initdatabase.sql

--- scripts/initdatabase.sql
+++ scripts/initdatabase.sql
@@ -2,18 +2,24 @@
 -- Step 1️⃣: Create Database
 -- ===============================
 
--- Drop the database if it already exists to start fresh
+-- Önce postgres DB’ye bağlı ol
+\c postgres
+
+-- Drop the database if it already exists
 DROP DATABASE IF EXISTS "DataWarehouse";
 
 -- Create a new database
 CREATE DATABASE "DataWarehouse";
+
+-- Bağlan DataWarehouse DB'ye
+\c "DataWarehouse"
 
 -- ===============================
 -- Step 2️⃣: Create Schemas
 -- ===============================
 
 -- Gold schema: for curated, analysis-ready data
-DROP SCHEMA IF EXISTS gold CASCADE;  -- CASCADE drops all tables inside the schema
+DROP SCHEMA IF EXISTS gold CASCADE;
 CREATE SCHEMA gold;
 
 -- Silver schema: for cleaned or processed data
